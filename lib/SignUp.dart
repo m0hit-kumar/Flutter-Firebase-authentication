7b333ceca7f5ef525af29c24253b8b20cde10c3c
Mohit kumar <60257288+m0hit-kumar@users.noreply.github.com>
2021-11-02 13:40:31 +0530

    init

---
+++ lib/SignUp.dart
@@ -0,0 +1,30 @@
+// import 'package:flutter/material.dart';
+
+// class SignUp extends StatefulWidget {
+//   @override
+//   _SignUpState createState() => _SignUpState();
+// }
+
+// class _SignUpState extends State<SignUp> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(),
+//       body: Center(
+//         child: Container(
+//             child: Column(
+//           children: [
+//             Row(
+//               children: [
+//                 Text("Name"),
+//                 TextField(),
+//               ],
+//             ),
+//             Text("Email"),
+//             TextField()
+//           ],
+//         )),
+//       ),
+//     );
+//   }
+// }
